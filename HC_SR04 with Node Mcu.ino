918671a5e76f0da1243698afadf997b895c488d8
Art Garcia <art.garcia@live.com>
2017-03-26 19:46:33 -0400

    removed duplicate includes

--- HC_SR04 with Node Mcu.ino
+++ HC_SR04 with Node Mcu.ino
@@ -12,10 +12,6 @@ NODE MCU .9 esp 8266 and hc-sr04 distance sensor
 #include <wire.h>
 
 
-// need this lib for Secure SSL for ESP 8266 chip
-#include <WiFiClientSecure.h>  
-
-
 // common include file with additional user functions ise 
 // To use tabs with a .h extension, you need to #include it (using "double quotes" not <angle brackets>).                    
 #include "Common.h"
@@ -44,7 +40,7 @@ void setup() {
 	pinMode(TRIGGER, OUTPUT);
 	pinMode(ECHO, INPUT);
 
-	Wire.pins(0, 2);
+	//Wire.pins(0, 2);
 	//lcd.begin(16, 2);
 	//lcd.println("hello");
 
